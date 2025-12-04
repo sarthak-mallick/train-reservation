2bcf7ff8a465d6a0402de767498acabb3115fa2c
Nishanth5464 <nishunishanth82@gmail.com>
2025-12-04 13:03:27 -0500

    Updated Test_Train.sql

--- Tests/Test_Train.sql
+++ Tests/Test_Train.sql
@@ -116,14 +116,15 @@ DECLARE
   v_msg VARCHAR2(200);
   v_ok  BOOLEAN;
 BEGIN
-  v_ok := PKG_TRAIN.update_fare(9999, 1500, 900, v_msg);
+  PKG_TRAIN.add_train_to_schedule(1, 1, v_ok, v_msg);
 
   DBMS_OUTPUT.PUT_LINE(
-    'Success: ' || CASE WHEN v_ok THEN 'TRUE' ELSE 'FALSE' END
+    'Success: ' || CASE WHEN v_ok THEN 'TRUE' ELSE 'FALSE' END ||
+    ', Msg: ' || v_msg
   );
-  DBMS_OUTPUT.PUT_LINE('Msg: ' || v_msg);
 END;
 /
+
 
 -- Case 3B — Duplicate add
 
@@ -140,6 +141,18 @@ BEGIN
 END;
 /
 
+-- Test Case 3C — Schedule does not exist --
+
+DECLARE
+  v_msg VARCHAR2(200);
+  v_ok  BOOLEAN;
+BEGIN
+  PKG_TRAIN.add_train_to_schedule(1, 999, v_ok, v_msg);
+
+  DBMS_OUTPUT.PUT_LINE('Success: ' || CASE WHEN v_ok THEN 'TRUE' ELSE 'FALSE' END);
+  DBMS_OUTPUT.PUT_LINE('Msg: ' || v_msg);
+END;
+/
 
 -- TEST 4 — cancel_train_on_date()
 
@@ -261,6 +274,155 @@ BEGIN
   DBMS_OUTPUT.PUT_LINE('Remove Schedule (Bookings Active) → ' || v_msg);
 END;
 /
+
+
+
+-- TEST 9 — delete_train
+
+-- Test Case 9A — Missing mandatory fields
+
+DECLARE
+  v_err VARCHAR2(200);
+  v_id NUMBER;
+BEGIN
+  v_id := PKG_TRAIN.add_train(
+    NULL, 'CHENNAI', 'BANGALORE',
+    10, 20, 1000, 500,
+    v_err
+  );
+
+  DBMS_OUTPUT.PUT_LINE('ID: ' || v_id);
+  DBMS_OUTPUT.PUT_LINE('Msg: ' || v_err);
+END;
+/
+
+
+-- Test Case 9B — Invalid seat counts --
+
+DECLARE
+  v_err VARCHAR2(200);
+  v_id NUMBER;
+BEGIN
+  v_id := PKG_TRAIN.add_train(
+    'A102', 'CHENNAI', 'BANGALORE',
+    0, 20, 1000, 500,
+    v_err
+  );
+
+  DBMS_OUTPUT.PUT_LINE('ID: ' || v_id);
+  DBMS_OUTPUT.PUT_LINE('Msg: ' || v_err);
+END;
+/
+
+-- Test Case 9C — Invalid fare --
+
+DECLARE
+  v_err VARCHAR2(200);
+  v_id NUMBER;
+BEGIN
+  v_id := PKG_TRAIN.add_train(
+    'A103', 'CHENNAI', 'BANGALORE',
+    10, 20, -100, 500,
+    v_err
+  );
+
+  DBMS_OUTPUT.PUT_LINE('ID: ' || v_id);
+  DBMS_OUTPUT.PUT_LINE('Msg: ' || v_err);
+END;
+/
+
+
+    -- 10. Add missing negative cases for update_fare --
+
+-- 10A Invalid new fare --
+
+DECLARE
+  v_ok BOOLEAN;
+  v_msg VARCHAR2(200);
+BEGIN
+  v_ok := PKG_TRAIN.update_fare(1, -200, 100, v_msg);
+
+  DBMS_OUTPUT.PUT_LINE('Success: ' || CASE WHEN v_ok THEN 'TRUE' ELSE 'FALSE' END);
+  DBMS_OUTPUT.PUT_LINE('Msg: ' || v_msg);
+END;
+/
+
+
+-- 11. Add missing cases for cancel_train_on_date --
+
+-- 11A. Train does not exist --
+
+DECLARE
+  v_cnt NUMBER;
+  v_ok  BOOLEAN;
+  v_msg VARCHAR2(200);
+BEGIN
+  PKG_TRAIN.cancel_train_on_date(
+    9999,
+    DATE '2025-12-10',
+    'Check',
+    v_cnt,
+    v_ok,
+    v_msg
+  );
+
+  DBMS_OUTPUT.PUT_LINE('Success: ' ||
+                       CASE WHEN v_ok THEN 'TRUE' ELSE 'FALSE' END);
+
+  DBMS_OUTPUT.PUT_LINE('Bookings Cancelled: ' || v_cnt);
+  DBMS_OUTPUT.PUT_LINE('Msg: ' || v_msg);
+END;
+/
+
+-- 11B. Train does NOT operate on that date --
+
+DECLARE
+  v_cnt NUMBER;
+  v_ok BOOLEAN;
+  v_msg VARCHAR2(200);
+BEGIN
+  PKG_TRAIN.cancel_train_on_date(
+    1,
+    DATE '2025-12-11',  -- choose wrong weekday
+    'Not operating',
+    v_cnt,
+    v_ok,
+    v_msg
+  );
+
+  DBMS_OUTPUT.PUT_LINE('Msg: ' || v_msg);
+END;
+/
+
+
+
+    -- 12. Add required tests for remove_train_from_schedule --
+    
+    
+-- 12A. Removing non-existent schedule --
+
+DECLARE
+  v_ok  BOOLEAN;
+  v_msg VARCHAR2(200);
+BEGIN
+  PKG_TRAIN.remove_train_from_schedule(1, 999, v_ok, v_msg);
+
+  DBMS_OUTPUT.PUT_LINE(
+    'Success: ' || CASE WHEN v_ok THEN 'TRUE' ELSE 'FALSE' END
+  );
+
+  DBMS_OUTPUT.PUT_LINE('Msg: ' || v_msg);
+END;
+/
+
+
+
+
+
+
+
+
+
 
 
 
